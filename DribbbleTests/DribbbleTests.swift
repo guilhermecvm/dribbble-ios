971142fffccb5b9d58c6bff07c5d475d69f1a85c
Guilherme Miranda <guimiranda@live.com>
2015-03-21 14:49:55 -0300

    initial commit

---
+++ DribbbleTests/DribbbleTests.swift
@@ -0,0 +1,36 @@
+//
+//  DribbbleTests.swift
+//  DribbbleTests
+//
+//  Created by Guilherme Miranda on 3/21/15.
+//  Copyright (c) 2015 Guilherme Miranda. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DribbbleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
